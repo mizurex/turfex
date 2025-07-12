924eb5bfa558811b98b4d5dac04928c7251e00cb
mizurex <viswasdhal@gmail.com>
2025-07-12 16:54:52 +0530

    issue solve

--- turfex/Client/src/main.jsx
+++ turfex/Client/src/main.jsx
@@ -1,4 +1,4 @@
-import { StrictMode } from 'react'
+
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
@@ -10,10 +10,10 @@ const clerk_key = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
 if(!clerk_key) throw new Error("Provide clerk key");
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
+
     <ClerkProvider publishableKey={clerk_key}
      >
       <App />
     </ClerkProvider>
-  </StrictMode>,
+ 
 )
