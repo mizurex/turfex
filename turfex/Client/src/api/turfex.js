c3d8b4f838851f93bb2112f32164e03b4f407c00
mizurex <viswasdhal@gmail.com>
2025-07-12 16:43:47 +0530

    small change

--- turfex/Client/src/api/turfex.js
+++ turfex/Client/src/api/turfex.js
@@ -1,5 +1,5 @@
 export const turfexAi = async(messages,tone,length,level,language)=>{
-    const res = await fetch('http://localhost:3001/api/turfex', {
+    const res = await fetch('https://turfex-5.onrender.com', {
     method: 'POST',
     headers: { 'Content-Type': 'application/json' },
     body: JSON.stringify({ messages,tone,length,level,language}),
